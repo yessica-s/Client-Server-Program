444e7ae4739d12a15af296fa21a2b8b0d3d02d07
yessica-s <153290587+yessica-s@users.noreply.github.com>
2025-04-25 13:00:59 +1000

    redo

--- chatclient.py
+++ chatclient.py
@@ -2,6 +2,8 @@ import sys
 from socket import *
 from sys import stdout, stdin, argv, exit
 
+BUFSIZE = 1024
+
 
 def usage_checking(arr):
     if len(sys.argv) < 3 or len(sys.argv) > 3:  # Not enough/too many arguments
@@ -13,13 +15,14 @@ def usage_checking(arr):
         exit(3)
 
     # TODO: NOT SURE IF THIS SHOULD BE DONE HERE?? SINCE PORT NUUMBER NOT CHECKED AS INT YET
-    if sys.argv[1] < 1024 or sys.argv[1] > 65535:  # port number out of range
+    if int(sys.argv[1]) < 1024 or int(sys.argv[1]) > 65535:  # port number out of range
         print("Usage: chatclient port_number client_username\n", file=sys.stderr)
         exit(3)
 
     return
 
-def check_port(port):
+
+def start_connection(port):
     # Check port number is integer
     if not port.isdigit():
         print(f"Error: Unable to connect to port {port}.\n", file=sys.stderr)
@@ -27,24 +30,21 @@ def check_port(port):
 
     # Check chatclient can connect to server on socket
     hostname = "localhost"
-    sock = socket(AF_INET, SOCK_STREAM)
     try:
+        sock = socket(AF_INET, SOCK_STREAM)
         sock.connect((hostname, port))
+        return sock # return the created socket
     except Exception:
         print(f"Error: Unable to connect to port {port}.\n", file=sys.stderr)
         exit(7)
 
-
-def check_username(client):
-    # TODO: NEED TO IMPLEMENT BASED ON SERVER PROTOCOL
-    pass
-
 def main():
     usage_checking(sys.argv)
-    port = sys.argv[1]
-    client = sys.argv[2]
-    check_port(port)
-    check_username(client)
+    port = int(sys.argv[1])
+    client_username = sys.argv[2]
+    sock = start_connection(port) # returns connected socket to send stuff on
+    sock.send(client_username.encode()) # send username to server
+    data = sock.recv(BUFSIZE) # receive server response to username
 
 
 if __name__ == "__main__":
